bef196922f32c73a15000fee5beefdcf74c9efbe
Lucas Nunes <4321beyblade@gmail.com>
2023-07-25 20:38:12 -0300

    Início do projeto

---
+++ todo-list-app/src/models/TodoList.ts
@@ -0,0 +1,11 @@
+import { Tarefa } from "../interfaces/tarefa";
+
+export class TodoList {
+    private tarefas: Array<Tarefa>;
+
+    constructor() {
+        this.tarefas = [];
+    }
+
+    // Funcionalidades
+}
