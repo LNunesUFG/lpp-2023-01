4b06659f2662ba738430b605bebfb18d1229f41e
sophia_almeida@discente.ufg.br <sophialmeida03@gmail.com>
2023-07-30 12:32:19 -0300

    Adicionando função filtrar e html

--- todo-list-app/src/ts/controllers/TodoListController.ts
+++ todo-list-app/src/ts/controllers/TodoListController.ts
@@ -59,4 +59,43 @@ export class TodoList {
     this.tarefas.sort();
   }
 
+  // Filtrar tarefas com base no estado de conclusão (completa/incompleta)
+  filtrarTarefasPorEstado(completa: boolean): Array<Tarefa> {
+    return this.tarefas.filter((tarefa) => tarefa.completa === completa);
+  }
+
+
+  // novoElemento() {
+  //   const li: HTMLLIElement = document.createElement("li");
+  //   const inputValor: string = (<HTMLInputElement>document.getElementById("inputTarefa")).value;
+  //   const texto: Text = document.createTextNode(inputValor);
+  //   li.appendChild(texto);
+
+  //   if (inputValor === "") {
+  //     alert("Escreva algo para adicionar! :)");
+  //     return; 
+  //   } else {
+  //     const ul: HTMLUListElement | null = document.getElementById("listaTarefas") as HTMLUListElement;
+  //     if(ul){
+  //       ul.appendChild(li);
+  //     }
+  //   }
+
+  //   (document.getElementById("inputTarefa") as HTMLInputElement).value = "";
+
+  //   const span: HTMLSpanElement = document.createElement("SPAN");
+  //   const txt: Text = document.createTextNode("\u00D7");
+  //   span.className = "close";
+  //   span.appendChild(txt);
+  //   li.appendChild(span);
+
+  //   span.onclick = function() {
+  //     const div: HTMLElement = span.parentElement as HTMLElement;
+  //     div.style.display = "none";
+  //   };
+
+  // } tentativa mal sucedida :(
+
+
+
 }
