4b94452af641d729f647ab4a5b86e6990c4a02ff
Lucas Nunes <4321beyblade@gmail.com>
2023-08-01 22:34:08 -0300

    Nova organização do código

--- todo-list-app/src/TodoListController.ts
+++ todo-list-app/src/TodoListController.ts
@@ -1,4 +1,4 @@
-import { Tarefa } from "../models/tarefa.model";
+import { Tarefa } from "./tarefa.model";
 
 export class TodoList {
   private tarefas: Array<Tarefa>;
@@ -63,39 +63,5 @@ export class TodoList {
   filtrarTarefasPorEstado(completa: boolean): Array<Tarefa> {
     return this.tarefas.filter((tarefa) => tarefa.completa === completa);
   }
-
-
-  // novoElemento() {
-  //   const li: HTMLLIElement = document.createElement("li");
-  //   const inputValor: string = (<HTMLInputElement>document.getElementById("inputTarefa")).value;
-  //   const texto: Text = document.createTextNode(inputValor);
-  //   li.appendChild(texto);
-
-  //   if (inputValor === "") {
-  //     alert("Escreva algo para adicionar! :)");
-  //     return; 
-  //   } else {
-  //     const ul: HTMLUListElement | null = document.getElementById("listaTarefas") as HTMLUListElement;
-  //     if(ul){
-  //       ul.appendChild(li);
-  //     }
-  //   }
-
-  //   (document.getElementById("inputTarefa") as HTMLInputElement).value = "";
-
-  //   const span: HTMLSpanElement = document.createElement("SPAN");
-  //   const txt: Text = document.createTextNode("\u00D7");
-  //   span.className = "close";
-  //   span.appendChild(txt);
-  //   li.appendChild(span);
-
-  //   span.onclick = function() {
-  //     const div: HTMLElement = span.parentElement as HTMLElement;
-  //     div.style.display = "none";
-  //   };
-
-  // } tentativa mal sucedida :(
-
-
 
 }
